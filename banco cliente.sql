5213bee6b583b834e79ccfa92b7e55b233bdd6ab
Rodolpho Galerani <99619762+Rooh0007@users.noreply.github.com>
2026-02-22 23:50:08 -0300

    Add files via upload
    Sistema de Cadastro de Clientes (MySQL)
    
    Este repositório contém um projeto simples de banco de dados desenvolvido para fins de estudo utilizando MySQL.
    
    🎯 Objetivo
    
    Praticar conceitos fundamentais de banco de dados, como:
    
    Criação de banco de dados
    
    Criação de tabelas
    
    Tipos de dados (INT, VARCHAR)
    
    Chaves primárias (PRIMARY KEY)
    
    AUTO_INCREMENT
    
    Inserção de dados (INSERT)
    
    Consultas com SELECT
    
    Atualização de registros (UPDATE)
    
    Exclusão de dados (DELETE)

---
+++ banco cliente.sql
@@ -0,0 +1,15 @@
+use cadastro_declientes;
+
+create table clientes(
+ id int auto_increment primary key,
+ nome varchar(100) not null,
+ email varchar(150) not null,
+ telefone varchar(20)
+ );
+ insert into clientes(nome, email, telefone)
+ value(
+ 'joão silva','joao@silva','+55 11 7070-7070'),
+ ('rodolpho galerani','rodolpho@gmail.com','+55 11 9999-9999'),
+ ('theo teofilo','theo@gmail.com','+55 11 5050-5050'),
+ ('theina teofilo','theina@hotmail.com','+55 13 1010-2020'),
+ ('clarice galerani','clarice@hotmail.com','+55 13 2525-3535');
